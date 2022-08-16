9924133b71d4b2c4ed4e0b572d48b8749a20e0ae
daniel2tavares <daniel2tavares@hotmail.com>
2022-08-16 12:19:21 -0300

    estilizacao da progress bar

--- src/components/card-pokemon/CardPokemon.jsx
+++ src/components/card-pokemon/CardPokemon.jsx
@@ -1,28 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '../../components/card-pokemon/CardPokemon.css'
-import ListaPokemon from "../lista-pokemon/ListaPokemon";
+import api from "../../services/api";
+
 
 function CardPokemon(){
 
+   const [data2, setData2] = useState([]);
+   const [pokeActive, setPokeActive] = useState('bulbasaur')
+
+    function changePoke(){
+        setPokeActive('charmander')
+    }
+
+   useEffect(() => {
+      api.get(`pokemon/${pokeActive}`)
+          .then((response) => {
+              setData2(response.data);
+          })
+          .catch((error) => {
+              console.log('falta de conexao com a api')
+          })
+  },[pokeActive]);
+
     return(
         <div className="card-pokemon">
-         <img src=''></img>
-         <h2>Nome do Pokemon</h2>
+         <img src={data2.sprites.front_shiny}></img>
+         <h2>{data2.name}</h2>
             <div className="displayPokemon">
-                <div>TIPO 1</div>
-                <div>TIPO 2</div>
+                <div>{data2.types[0].type.name}</div>
+                <div>{data2.types[1].type.name}</div>
             </div>
             <div className="display-progress">
-               <h5>HP</h5> <p className="progress-bar"></p>
-            </div>
-            <div className="display-progress">
-               <h5>ATACK</h5><p className="progress-bar"></p>
-            </div>
-            <div className="display-progress">
-               <h5>DEFENSE</h5><p className="progress-bar"></p>
-            </div>
-            <div className="display-progress">
-               <h5>SPEED</h5><p className="progress-bar"></p>
+               <h5>{(data2.stats[0].stat.name).toUpperCase()}  <progress max ='100' value={data2.stats[0].base_stat} className="progress-bar"></progress></h5> 
+               <h5>{(data2.stats[1].stat.name).toUpperCase()}  <progress max ='100' value={data2.stats[1].base_stat} className="progress-bar"></progress></h5> 
+               <h5>{(data2.stats[2].stat.name).toUpperCase()}  <progress max ='100' value={data2.stats[2].base_stat} className="progress-bar"></progress></h5>
+               <h5>{(data2.stats[5].stat.name).toUpperCase()}  <progress max ='100' value={data2.stats[5].base_stat} className="progress-bar"></progress></h5>                    
             </div>
         </div>
               
--- src/components/lista-pokemon/ListaPokemon.jsx
+++ src/components/lista-pokemon/ListaPokemon.jsx
@@ -6,28 +6,23 @@ import ButtonPn from "../button-prev-nex/ButtonPn";
 function ListaPokemon() {
 
     const [data, setData] = useState([]);
-    const [data2, setData2] = useState([]);
     const [count, setCount] = useState(0);
-    const [pokeActive, setPokeActive] = useState('bulbasaur')
 
-    function avancarLista(){
-        
-        while(count < 1154){
+
+    function avancarLista() {
+
+        while (count < 1154) {
             setCount(count + 6)
         }
-        
+
     }
 
-    function voltarLista(){
-        if (count === 0){
+    function voltarLista() {
+        if (count === 0) {
             setCount(0)
-        }else{
+        } else {
             setCount(count - 6)
         }
-    }
-
-    function changePoke(){
-        setPokeActive('charmander')
     }
 
     useEffect(() => {
@@ -38,17 +33,7 @@ function ListaPokemon() {
             .catch((error) => {
                 console.log('falta de conexao com a api')
             })
-    },[count]);
-
-    useEffect(() => {
-        api.get(`pokemon/${pokeActive}`)
-            .then((response) => {
-                setData2(response.data);
-            })
-            .catch((error) => {
-                console.log('falta de conexao com a api')
-            })
-    },[pokeActive]);
+    }, [count]);
 
     return (
         <>
@@ -59,7 +44,7 @@ function ListaPokemon() {
                     data && data.results && data.results.map((result) => {
 
                         return (
-                            <h3 onClick={changePoke} key={result.name}>{result.name}</h3>
+                            <h3 key={result.name}>{result.name}</h3>
                         )
                     })
                 }
@@ -67,7 +52,7 @@ function ListaPokemon() {
             </div>
             <div className='buttons'>
                 <ButtonPn event={voltarLista} name='Prev' />
-                <ButtonPn event={avancarLista}  name='Next' />
+                <ButtonPn event={avancarLista} name='Next' />
             </div>
         </>
     )
